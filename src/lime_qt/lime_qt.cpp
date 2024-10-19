d291fdc06f2efe4a5faee67a600f9043aa3c058f
OpenSauce04 <opensauce04@gmail.com>
2024-10-20 00:05:37 +0100

    lime_qt: Added warning message when attempting to use `--multiplayer` option

--- src/lime_qt/lime_qt.cpp
+++ src/lime_qt/lime_qt.cpp
@@ -270,6 +270,16 @@ GMainWindow::GMainWindow(Core::System& system_)
                 exit(0);
             }
 
+            ubercase(QStringLiteral("--multiplayer")) : ubercase(QStringLiteral("-m")) : {
+                std::cout << "Warning: The --multiplayer option is not yet implemented for the Qt "
+                             "frontend; Ignoring."
+                          << std::endl;
+                if (i < args.size() - 1 && !args[i + 1].startsWith(QChar::fromLatin1('-'))) {
+                    i++;
+                }
+                continue;
+            }
+
             ubercase(QStringLiteral("--movie-play")) : ubercase(QStringLiteral("-p")) : {
                 if (i >= args.size() - 1 || args[i + 1].startsWith(QChar::fromLatin1('-'))) {
                     continue;
